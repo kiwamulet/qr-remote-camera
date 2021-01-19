dbf4e71743127b1d69018ab03115aca298465b32
Kiwamu Yoshioka <kiwamulet@gmail.com>
2021-01-19 19:19:24 +0900

    feat: show QR code for SENDER_URL
    use react-qrious

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,6 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
+import { QRious } from "react-qrious";
 
 const sender_url = process.env.SENDER_URL;
 
@@ -7,6 +8,7 @@ ReactDOM.render(
   <div>
     <h1>Sender URL:</h1>
     <div>{sender_url}</div>
+    <QRious value={sender_url} />
   </div>,
   document.getElementById("root")
 );
