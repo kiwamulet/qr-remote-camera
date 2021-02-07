07232546050badb4a6d8987e66fba27dc52c4e03
Kiwamu Yoshioka <kiwamulet@gmail.com>
2021-02-07 21:37:00 +0900

    chore: reorganize receiver components

--- src/pages/Receiver.tsx
+++ src/pages/Receiver.tsx
@@ -1,8 +1,15 @@
 import * as React from "react";
+import * as Ayame from "@open-ayame/ayame-web-sdk";
 import { QrLink } from "~/components/QrLink";
-import { constructAppUrl } from "~/common/appUrlUtils";
-import { ReceivingVideo } from "~/components/ReceivingVideo";
+import { Video } from "~/components/Video";
 import { SIGNALING_URL } from "~/common/constants";
+import { constructAppUrl } from "~/common/appUrlUtils";
+
+// type Props = {
+//   signalingKey?: string;
+//   roomId: string;
+//   signalingUrl: string;
+// };
 
 function getUniqueStr() {
   return (
@@ -17,24 +24,70 @@ type Props = {
   signalingKey?: string;
 };
 
-const Receiver = (props: Props) => {
-  const roomId = `${props.baseRoomId}@${getUniqueStr()}`;
-  const senderUrl = constructAppUrl(
-    props.baseSenderUrl,
-    roomId,
-    props.signalingKey
-  );
-  return (
-    <div>
-      <QrLink sender_url={senderUrl.href} />
-      <br />
-      <ReceivingVideo
-        signalingKey={props.signalingKey}
-        roomId={roomId}
-        signalingUrl={SIGNALING_URL}
-      />
-    </div>
-  );
-};
+type State = { srcObject: MediaStream | null };
+
+class Receiver extends React.Component<Props, State> {
+  readonly roomId: string;
+  readonly senderUrl: URL;
+
+  constructor(props: Props) {
+    super(props);
+    this.state = { srcObject: null };
+    const { signalingKey, baseRoomId } = this.props;
+    this.roomId = `${baseRoomId}@${getUniqueStr()}`;
+
+    this.senderUrl = constructAppUrl(
+      props.baseSenderUrl,
+      this.roomId,
+      props.signalingKey
+    );
+  }
+
+  componentDidMount() {
+    const { signalingKey, baseRoomId } = this.props;
+    if (signalingKey) {
+      this.startConnection(SIGNALING_URL, signalingKey, this.roomId);
+    }
+  }
+
+  componentWillUnmount() {}
+
+  async startConnection(
+    signalingUrl: string,
+    signalingKey: string,
+    roomId: string
+  ) {
+    const options = Ayame.defaultOptions;
+    options.signalingKey = signalingKey;
+    options.video.direction = "recvonly";
+    options.audio.direction = "recvonly";
+    options.audio.enabled = false;
+    const connection = Ayame.connection(signalingUrl, roomId, options, true);
+    await connection.connect(null);
+    connection.on("disconnect", (e: object) => {
+      console.log("conn.disconnect", e);
+      this.setState({
+        srcObject: null,
+      });
+    });
+    connection.on("addstream", (e: any) => {
+      // todo: e seems to be RTCTrackEvent, but RTCTrackEvent does not have "stream" property
+      console.log("conn.addstream", e);
+      this.setState({
+        srcObject: e.stream,
+      });
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <QrLink sender_url={this.senderUrl.href} />
+        <br />
+        <Video srcObject={this.state.srcObject} autoPlay />
+      </div>
+    );
+  }
+}
 
 export { Receiver };
